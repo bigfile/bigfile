250a3996137e9fc7ea26909ff7191271601d8402
Michael <1185694600@qq.com>
2019-09-01 17:09:17 +0800

    feat(rpc): add usage example for rpc

--- rpc/server.go
+++ rpc/server.go
@@ -2,6 +2,7 @@
 //  Use of this source code is governed by a MIT-style
 //  license that can be found in the LICENSE file.
 
+// Package rpc is used to implement rpc protocol
 package rpc
 
 import (
