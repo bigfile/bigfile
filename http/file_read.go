051d99ae1330f223d7d63e4566f3566d1ecfd86c
Michael <1185694600@qq.com>
2019-09-05 23:05:21 +0800

    feat(http): implement http range protocol

--- http/file_read.go
+++ http/file_read.go
@@ -6,13 +6,16 @@ package http
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"mime"
 	"net/http"
 	"path"
 	"reflect"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/bigfile/bigfile/databases/models"
@@ -20,6 +23,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
+
+// ErrWrongRangeHeader represent the http range header format error
+var ErrWrongRangeHeader = errors.New("http range header format error")
+
+// ErrWrongHttpRange represent wrong http range header
+var ErrWrongHttpRange = errors.New("wrong http range header, start must be less than end")
+
+const binaryContentType = "application/octet-stream"
 
 type fileReadInput struct {
 	Token         string  `form:"token" binding:"required"`
@@ -32,16 +43,16 @@ type fileReadInput struct {
 // FileReadHandler is used to handle file read request
 func FileReadHandler(ctx *gin.Context) {
 	var (
-		ip                     = ctx.ClientIP()
-		db                     = ctx.MustGet("db").(*gorm.DB)
-		err                    error
-		file                   *models.File
-		token                  = ctx.MustGet("token").(*models.Token)
-		input                  = ctx.MustGet("inputParam").(*fileReadInput)
-		requestID              = ctx.GetInt64("requestId")
-		fileReadSrv            *service.FileRead
-		fileReadSrvValue       interface{}
-		fileReadSrvValueReader io.ReadSeeker
+		ip               = ctx.ClientIP()
+		db               = ctx.MustGet("db").(*gorm.DB)
+		err              error
+		file             *models.File
+		token            = ctx.MustGet("token").(*models.Token)
+		input            = ctx.MustGet("inputParam").(*fileReadInput)
+		requestID        = ctx.GetInt64("requestId")
+		fileReadSrv      *service.FileRead
+		fileReaderSeeker io.ReadSeeker
+		fileReadSrvValue interface{}
 	)
 
 	if file, err = models.FindFileByUID(input.FileUID, false, db); err != nil {
@@ -54,12 +65,10 @@ func FileReadHandler(ctx *gin.Context) {
 	}
 
 	fileReadSrv = &service.FileRead{
-		BaseService: service.BaseService{
-			DB: db,
-		},
-		Token: token,
-		File:  file,
-		IP:    &ip,
+		BaseService: service.BaseService{DB: db},
+		Token:       token,
+		File:        file,
+		IP:          &ip,
 	}
 
 	if isTesting {
@@ -83,15 +92,52 @@ func FileReadHandler(ctx *gin.Context) {
 		})
 		return
 	}
-	fileReadSrvValueReader = fileReadSrvValue.(io.ReadSeeker)
-	readAllContents(ctx, fileReadSrvValueReader, file, input)
+	fileReaderSeeker = fileReadSrvValue.(io.ReadSeeker)
+	rangeHeader := ctx.Request.Header.Get("Range")
+	if rangeHeader == "" {
+		readAllContent(ctx, fileReaderSeeker, file, input)
+		return
+	}
+	rangeHeaderPattern := regexp.MustCompile(`^bytes=(?P<start>\d*)-(?P<end>\d*)$`)
+	if !rangeHeaderPattern.Match([]byte(rangeHeader)) {
+		ctx.JSON(400, &Response{
+			RequestID: requestID,
+			Success:   false,
+			Errors:    generateErrors(ErrWrongRangeHeader, ""),
+		})
+		return
+	}
+	rangePosition := strings.TrimPrefix(rangeHeader, "bytes=")
+	rangeStart := 0
+	rangeEnd := file.Size
+	if rangePosition == "-" {
+		readAllContent(ctx, fileReaderSeeker, file, input)
+		return
+	} else if strings.HasPrefix(rangePosition, "-") {
+		rangeEnd, _ = strconv.Atoi(strings.TrimPrefix(rangePosition, "-"))
+	} else if strings.HasSuffix(rangePosition, "-") {
+		rangeStart, _ = strconv.Atoi(strings.TrimSuffix(rangePosition, "-"))
+	} else {
+		rangePositionSplit := strings.Split(rangePosition, "-")
+		rangeStart, _ = strconv.Atoi(rangePositionSplit[0])
+		rangeEnd, _ = strconv.Atoi(rangePositionSplit[1])
+	}
+	if rangeStart > rangeEnd {
+		ctx.JSON(400, &Response{
+			RequestID: requestID,
+			Success:   false,
+			Errors:    generateErrors(ErrWrongHttpRange, ""),
+		})
+		return
+	}
+	readRangeContent(ctx, fileReaderSeeker, file, input, rangeStart, rangeEnd)
 }
 
-func readAllContents(ctx *gin.Context, readerSeeker io.ReadSeeker, file *models.File, input *fileReadInput) {
+func readAllContent(ctx *gin.Context, readerSeeker io.ReadSeeker, file *models.File, input *fileReadInput) {
 	headers := map[string]string{
 		"ETag":                file.Object.Hash,
 		"Accept-Ranges":       "bytes",
-		"Content-Type":        "application/octet-stream",
+		"Content-Type":        binaryContentType,
 		"Last-Modified":       file.UpdatedAt.Format(time.RFC1123),
 		"Content-Disposition": fmt.Sprintf(`attachment; filename="%s"`, file.Name),
 	}
@@ -104,4 +150,21 @@ func readAllContents(ctx *gin.Context, readerSeeker io.ReadSeeker, file *models.
 	}
 	ctx.Set("ignoreRespBody", true)
 	ctx.DataFromReader(http.StatusOK, int64(file.Size), headers["Content-Type"], readerSeeker, headers)
+}
+
+func readRangeContent(ctx *gin.Context, readerSeeker io.ReadSeeker, file *models.File, input *fileReadInput, start, end int) {
+	if _, err := readerSeeker.Seek(int64(start), io.SeekStart); err != nil {
+		ctx.JSON(400, &Response{
+			RequestID: ctx.GetInt64("requestId"),
+			Success:   false,
+			Errors:    generateErrors(err, ""),
+		})
+		return
+	}
+	limitSize := int64(end - start)
+	limitReader := io.LimitReader(readerSeeker, limitSize)
+	ctx.Set("ignoreRespBody", true)
+	ctx.DataFromReader(http.StatusPartialContent, limitSize, binaryContentType, limitReader, map[string]string{
+		"Content-Range": fmt.Sprintf("%d-%d/%d", start, end, file.Size),
+	})
 }
