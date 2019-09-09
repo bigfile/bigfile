de5116dae6170f870505e599483ee579b7d512f5
Michael <1185694600@qq.com>
2019-09-09 23:01:50 +0800

    update: .goreleaser.yml

--- scripts/goreleaser.sh
+++ scripts/goreleaser.sh
@@ -1,29 +1,9 @@
 #!/bin/sh
 set -e
 
-TAR_FILE="/tmp/goreleaser.tar.gz"
-RELEASES_URL="https://github.com/goreleaser/goreleaser/releases"
-test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
-
-last_version() {
-  curl -sL -o /dev/null -w %{url_effective} "$RELEASES_URL/latest" |
-    rev |
-    cut -f1 -d'/'|
-    rev
-}
-
-download() {
-  test -z "$VERSION" && VERSION="$(last_version)"
-  test -z "$VERSION" && {
-    echo "Unable to get goreleaser version." >&2
-    exit 1
-  }
-  rm -f "$TAR_FILE"
-  curl -s -L -o "$TAR_FILE" \
-    "$RELEASES_URL/download/$VERSION/goreleaser_$(uname -s)_$(uname -m).tar.gz"
-}
-
-download
-tar -xf "$TAR_FILE" -C "$TMPDIR"
 git status
-"${TMPDIR}/goreleaser" "$@"
+git add .
+git commit -m "submit local update for release"
+git status
+
+curl -sL https://git.io/goreleaser | bash
